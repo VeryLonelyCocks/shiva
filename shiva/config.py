732f2db83a1398d8fd2ba0c915c5c6a34409da73
Taly <vitalik7tv@yandex.ru>
2017-10-21 05:02:30 +0300

    remove config

--- shiva/config.py
+++
@@ -1,9 +0,0 @@
-config = {
-    'host': '01d826e4.ngrok.io',
-    'core': {
-        'port': 3000,
-        'name': 'test',
-        'hawk': '9ff64221-f38c-4714-b0d0-097770d07941',
-        'telegram_token': '321562142:AAGnykpekhExR3DDfZcjbOzEaqE8fya-DlU'
-    }
-}
